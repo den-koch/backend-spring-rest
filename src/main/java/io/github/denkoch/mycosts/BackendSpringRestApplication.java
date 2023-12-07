50fd5fb1163fa93629f859a029020115db83ef34
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-07 22:00:25 +0200

    [Add] app inits

---
+++ src/main/java/io/github/denkoch/mycosts/BackendSpringRestApplication.java
@@ -0,0 +1,34 @@
+package io.github.denkoch.mycosts;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Home Accounting App",
+                contact = @Contact(
+                        name = "Denys Kochetov",
+                        email = "@gmail.com"
+                ),
+                description = "Home accounting app",
+                version = "1.13",
+                license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "test server")
+        }
+)
+
+@SpringBootApplication
+public class BackendSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/denkoch/mycosts/BackendSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.denkoch.mycosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
