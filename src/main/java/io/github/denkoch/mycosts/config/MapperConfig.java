dee11a3801db0867259a12a5240b375b7d14c71b
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-30 00:40:48 +0200

    [Delete] custom mappers

--- src/main/java/io/github/denkoch/mycosts/config/MapperConfig.java
+++ src/main/java/io/github/denkoch/mycosts/config/MapperConfig.java
@@ -1,27 +1,18 @@
 package io.github.denkoch.mycosts.config;
 
-import io.github.denkoch.mycosts.config.mappers.CategoryMapper;
-import io.github.denkoch.mycosts.config.mappers.PaymentMapper;
-import io.github.denkoch.mycosts.config.mappers.UserMapper;
+import io.github.denkoch.mycosts.category.models.Category;
+import io.github.denkoch.mycosts.category.models.CategoryRequestDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MapperConfig {
 
     @Bean
-    public UserMapper userMapper(){
-        return new UserMapper();
-    }
-
-    @Bean
-    public PaymentMapper paymentMapper(){
-        return new PaymentMapper();
-    }
-
-    @Bean
-    public CategoryMapper categoryMapper(){
-        return new CategoryMapper();
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
